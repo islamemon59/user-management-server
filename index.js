43dd8252ade03cad472190fff47c38eb76896928
islamemon59 <islamemon59@gmail.com>
2025-05-16 13:32:22 +0600

    user update inforamtin pathc added

--- index.js
+++ index.js
@@ -55,6 +55,20 @@ async function run() {
         res.send(result)
     })
 
+    //update user data in database
+    app.patch("/users/:id", async (req, res) => {
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)}
+        const {email} = req.body
+        const updatedDoc = {
+            $set: {
+                email:email
+            }
+        }
+        const result = await userCollection.updateOne(filter, updatedDoc)
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
